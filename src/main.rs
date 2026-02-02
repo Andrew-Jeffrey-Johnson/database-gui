844a7dd68a8bea72f49cddf9e31a1ded0d44d5a1
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-02-01 16:59:50 -0800

    Basic PostgreSQL setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+use dotenv::dotenv;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result {
+#[tokio::main]
+async fn main() -> eframe::Result {
+    // PostgreSQL
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool.");
+
+    println!("Connected to the database!");
+
+    // egui
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
