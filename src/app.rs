ecfbc06795501922962899dc8809689a34f4a460
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-02-07 10:05:33 -0800

    Removed github link

--- src/app.rs
+++ src/app.rs
@@ -133,11 +133,6 @@ impl eframe::App for TemplateApp {
 
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
